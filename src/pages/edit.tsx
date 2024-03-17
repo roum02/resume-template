c3d73c04569a286ed3e5df1d51415dbdab29e061
roum <roum02@ewhain.net>
2024-03-17 22:30:58 +0900

    Modify directories [main]

---
+++ src/pages/edit.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditPage = () => {
+    return (
+        <div>
+            <p>This is for edit page.</p>
+        </div>
+    );
+};
+
+export default EditPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-   <div>index page</div>
-  );
+    return (
+        <div>index page</div>
+    );
 }
