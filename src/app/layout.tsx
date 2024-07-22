bb383d61a1a913203d8fb631ba5eabb7be712939
roum02 <roum02@ewhain.net>
2024-07-22 23:57:36 +0900

    Add prettier [main]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
-import "../styles/globals.css";
+import { Inter } from 'next/font/google';
+import '../styles/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-import Image from "next/image";
-
 export default function IndexPage() {
-    return (
-        <div>index page</div>
-    );
+  // this page will be main page that could see the whole contents of resume.
+  return (
+    <>
+      {/*title*/}
+      <div>이력서 템플릿 - 이름 | 한 줄 소개 (옵션)</div>
+      {/*introduce my self in one line*/}
+      <div>한 줄 자기소개를 입력해주세요.</div>
+      <div>
+        <img />
+        <div>Contact</div>
+        <div>Channel</div>
+      </div>
+      <div>Introduce</div>
+      <div>Work Experience</div>
+      <div>Other Experience</div>
+      <div>Skill</div>≤<div>Education</div>
+    </>
+  );
 }
